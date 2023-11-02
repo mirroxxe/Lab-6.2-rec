221eaf3e084e2d4a6f2553cf71b9f52604af81f7
Volodya <chuchman002@gmail.com>
2023-11-02 18:05:52 +0200

    Add project files.

---
+++ Lab 6.2 rec/Lab 6.2 rec.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+int countnumbersRecursive(const std::vector<int>& a, int index) {
+    if (index < 0) {
+        return 0;
+    }
+
+    int count = countnumbersRecursive(a, index - 1);
+    if (a[index] % 2 != 0) {
+        count++;
+    }
+
+    return count;
+}
+
+int countnumbers(const std::vector<int>& a) {
+    return countnumbersRecursive(a, a.size() - 1);
+}
+
+int main() {
+    std::vector<int> a = { 2, 4, 7, 1, 9, 6, 3 };
+
+    int oddCount = countnumbers(a);
+
+    std::cout << "count of numbers: " << oddCount << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.2 rec/Lab 6.2 rec.cpp"
+#include <vector>
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			std::vector<int> a = { 2, 4, 7, 1, 9, 6, 3 };
+
+			int expectedCount = 4;  
+
+			int result = countnumbers(a);  
+
+			Assert::AreEqual(expectedCount, result);
+		}
+	};
+}
